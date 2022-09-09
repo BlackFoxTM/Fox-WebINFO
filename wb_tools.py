4db49b98b0e2d5721699cf7f9bfa24e1d1c2d81e
alireza <alirezaarzehgar82@gmail.com>
2022-09-09 16:12:24 +0430

    Remoev unused implementations
     - Remove unused comments
     - Remove unused variables
    
    Signed-off-by: alireza <alirezaarzehgar82@gmail.com>

--- wb_tools.py
+++ wb_tools.py
@@ -1,21 +1,18 @@
 #!/usr/bin/python3
-import requests
 import pyfiglet
-
+from requests import get, head
 from colorama import Fore
 from bs4 import BeautifulSoup
 
 
 if __name__ == '__main__':
-    # Print banner
     figlet = pyfiglet.Figlet(font="script")
     print(Fore.CYAN + figlet.renderText("WEB_INFO FOX"))
     print(f"{Fore.WHITE}[+] Coded By Maximum Radikali")
     print(f"{Fore.YELLOW}[+] Channel : @BlackFoxSecurityTeam")
     url = input(f"{Fore.BLUE}[&] Please Enter URL ex : (google.com) ~> ")
 
-    # Processed URL
-    resp = requests.get(f"https://w3techs.com/sites/info/{url}")
+    resp = get(f"https://w3techs.com/sites/info/{url}")
     soup = BeautifulSoup(resp.text, "html.parser")
     soupx = soup.find_all("div", class_="si_tech")
     alexa = soupx[0].text
@@ -25,21 +22,14 @@ if __name__ == '__main__':
     elif "Description on Homepage" in resp.text:
         alexa = soupx[1].text
 
-    # Find front_lang, library_lang, websv and wordpress
-    soupz = soup.find_all("p", {"class": "si_tech"})
-    front_lang = soupz[3].find("a").text
-    library_lang = soupz[4].find("a").text
-    websv = requests.head(f"https://{url}").headers.get("server")
-    resp = requests.get(f"https://{url}/wp-admin/")
     is_wordpress = f"{Fore.RED}No"
-
-    if resp.ok:
+    if get(f"https://{url}/wp-admin/").ok:
         is_wordpress = f"{Fore.GREEN}Yes"
 
-    # Show result
+    soupz = soup.find_all("p", {"class": "si_tech"})
     print(f'''{Fore.YELLOW}
 [-] Alexa Rank : {alexa}
-[-] Front Language : {front_lang}
-[-] Library Used : {library_lang}
-[-] Web Server : {websv}
+[-] Front Language : {soupz[3].find("a").text}
+[-] Library Used : {soupz[4].find("a").text}
+[-] Web Server : {head(f"https://{url}").headers.get("server")}
 [*] Wordpress : {is_wordpress}{Fore.WHITE}''')
