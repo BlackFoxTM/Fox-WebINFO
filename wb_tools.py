c69b7417e3c15574b3d068b7e912330474ff3bbc
Black Fox <109810908+BlackFoxTM@users.noreply.github.com>
2022-09-09 11:32:07 +0430

    Update wb_tools.py

--- wb_tools.py
+++ wb_tools.py
@@ -18,7 +18,7 @@ def banner():
 
 def checker(url):
     operation = builtwith.builtwith(url)
-    return ("[-] Web Server : %s\n[-] Font Script : %s\n[-] ecommerce : %s\n[-] CMS : %s\n[-] Programmed Lang With : %s\n[-] Blogs : %s" % (operation.get("web-servers")[0] , operation.get("font-scripts")[0] , operation.get("ecommerce")[0] , operation.get("cms")[0] , operation.get("programming-languages")[0] , operation.get("blogs")[0]))
+    return ("[-] Web Server : %s\n[-] Font Script : %s\n[-] ecommerce : %s\n[-] CMS : %s\n[-] Programmed Lang With : %s\n[-] Blogs : %s" % (str(operation.get("web-servers")[0]) , str(operation.get("font-scripts")[0]) , str(operation.get("ecommerce")[0]) , str(operation.get("cms")[0]) , str(operation.get("programming-languages")[0]) , str(operation.get("blogs")[0])))
 
 print (gn + banner())
 print (rd + "[$] Coded By Maximum Radikali")
