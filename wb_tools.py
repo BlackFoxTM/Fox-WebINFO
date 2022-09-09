2d48bb568296ad75bf69ac58547fb5074a299554
alireza <alirezaarzehgar82@gmail.com>
2022-09-09 15:11:46 +0430

    Remove aliases for `Fore.{COLOR}` variables.
     - Change import orders to PEP8 standard
     - Use string formatting instead of concatination
    
    Signed-off-by: alireza <alirezaarzehgar82@gmail.com>

--- wb_tools.py
+++ wb_tools.py
@@ -1,16 +1,9 @@
-import requests
-from bs4 import BeautifulSoup
 import re
-import colorama
+import requests
 import pyfiglet
 
-rd = colorama.Fore.RED
-cv = colorama.Fore.WHITE
-mag = colorama.Fore.MAGENTA
-gn = colorama.Fore.GREEN
-bl = colorama.Fore.BLUE
-yl = colorama.Fore.YELLOW
-cy = colorama.Fore.CYAN
+from colorama import Fore
+from bs4 import BeautifulSoup
 
 
 def banner():
@@ -21,32 +14,33 @@ def banner():
 #site = "https://w3techs.com/sites/info/000webhost.com"
 
 def checker(site):
-    print (yl)
+    print(Fore.YELLOW)
     lkas = "https://w3techs.com/sites/info/" + site
     req = requests.get(lkas).text
-    soup = BeautifulSoup(req , "html.parser")
-    soupx = soup.find_all("div",class_="si_tech")
-    if ("Online since"  in req) and ("Description on Homepage" in req):
+    soup = BeautifulSoup(req, "html.parser")
+    soupx = soup.find_all("div", class_="si_tech")
+    if ("Online since" in req) and ("Description on Homepage" in req):
         alexa = soupx[2].text
     elif "Description on Homepage" in req:
         alexa = soupx[1].text
     else:
         alexa = soupx[0].text
-    soupz = soup.find_all("p" , {"class":"si_tech"})
+    soupz = soup.find_all("p", {"class": "si_tech"})
     front_lang = soupz[3].find("a").text
     library_lang = soupz[4].find("a").text
     websv = requests.head("https://"+site).headers.get("server")
-    print ("[-] Alexa Rank : %s\n[-] Front Language : %s\n[-] Library Used : %s\n[-] Web Server : %s" % (alexa , front_lang , library_lang , websv))
+    print("[-] Alexa Rank : %s\n[-] Front Language : %s\n[-] Library Used : %s\n[-] Web Server : %s" %
+          (alexa, front_lang, library_lang, websv))
     wp = site + "/wp-admin/"
     status = requests.get("https://"+wp).status_code
     if status != 200:
-        print (f"[*] Wordpress : {rd}No{cv}")
+        print(f"[*] Wordpress : {Fore.RED}No{Fore.WHITE}")
     else:
-        print (f"[*] Wordpress : {gn}Yes{cv}")
+        print(f"[*] Wordpress : {Fore.GREEN}Yes{Fore.WHITE}")
 
 
-print (cy + banner())
-print (mag + "[+] Coded By Maximum Radikali")
-print (yl + "[+] Channel : @BlackFoxSecurityTeam")
-url = input(bl + "[&] Please Enter URL ex : (google.com) ~> ")
+print(Fore.CYAN + banner())
+print(f"{Fore.WHITE}[+] Coded By Maximum Radikali")
+print(f"{Fore.YELLOW}[+] Channel : @BlackFoxSecurityTeam")
+url = input(f"{Fore.BLUE}[&] Please Enter URL ex : (google.com) ~> ")
 checker(url)
