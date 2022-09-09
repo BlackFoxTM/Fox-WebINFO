8656f929363a1ee6b43fc7fd4e8dd5ff5187e637
alireza <alirezaarzehgar82@gmail.com>
2022-09-09 15:47:05 +0430

    Split some logics to multi line and remove complexity
    Signed-off-by: alireza <alirezaarzehgar82@gmail.com>

--- wb_tools.py
+++ wb_tools.py
@@ -14,27 +14,34 @@ if __name__ == '__main__':
     print(f"{Fore.YELLOW}[+] Channel : @BlackFoxSecurityTeam")
     url = input(f"{Fore.BLUE}[&] Please Enter URL ex : (google.com) ~> ")
 
-    # Print URL information
-    print(Fore.YELLOW)
-    lkas = "https://w3techs.com/sites/info/" + url
-    req = requests.get(lkas).text
-    soup = BeautifulSoup(req, "html.parser")
+    # Processed URL
+    resp = requests.get(f"https://w3techs.com/sites/info/{url}")
+    soup = BeautifulSoup(resp.text, "html.parser")
     soupx = soup.find_all("div", class_="si_tech")
-    if ("Online since" in req) and ("Description on Homepage" in req):
+
+    # Find alexa rank
+    if "Online since" in resp.text and "Description on Homepage" in resp.text:
         alexa = soupx[2].text
-    elif "Description on Homepage" in req:
+    elif "Description on Homepage" in resp.text:
         alexa = soupx[1].text
     else:
         alexa = soupx[0].text
+
+    # Find front_lang, library_lang, websv and wordpress
     soupz = soup.find_all("p", {"class": "si_tech"})
     front_lang = soupz[3].find("a").text
     library_lang = soupz[4].find("a").text
-    websv = requests.head("https://"+url).headers.get("server")
-    print("[-] Alexa Rank : %s\n[-] Front Language : %s\n[-] Library Used : %s\n[-] Web Server : %s" %
-          (alexa, front_lang, library_lang, websv))
-    wp = url + "/wp-admin/"
-    status = requests.get("https://"+wp).status_code
-    if status != 200:
-        print(f"[*] Wordpress : {Fore.RED}No{Fore.WHITE}")
-    else:
-        print(f"[*] Wordpress : {Fore.GREEN}Yes{Fore.WHITE}")
+    websv = requests.head(f"https://{url}").headers.get("server")
+    resp = requests.get(f"https://{url}/wp-admin/")
+    is_wordpress = f"{Fore.RED}No"
+
+    if resp.ok:
+        is_wordpress = f"{Fore.GREEN}Yes"
+
+    # Show result
+    print(f'''{Fore.YELLOW}
+[-] Alexa Rank : {alexa}
+[-] Front Language : {front_lang}
+[-] Library Used : {library_lang}
+[-] Web Server : {websv}
+[*] Wordpress : {is_wordpress}{Fore.WHITE}''')
